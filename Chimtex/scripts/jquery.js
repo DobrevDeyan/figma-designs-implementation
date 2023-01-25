eabb75300604de37bec65f7eea9657134f9234b3
deyandobrev <dobrev.de@gmail.com>
2023-01-25 08:39:32 +0200

    home

--- Chimtex/scripts/jquery.js
+++ Chimtex/scripts/jquery.js
@@ -1,17 +1,15 @@
+// CLOS/OPEN FUNCTIONALITY PRODUCTS DROPDOWN
 $(".dropdown-menu-container").mouseenter(function () {
-  $(".megamenu").stop(true, true).slideDown(600);
-  $(".megamenu").css("display", "flex");
-  $(".dropdown-menu-container").css();
-});
+  $(".megamenu").stop(true, true).slideDown(600)
+  $(".megamenu").css("display", "flex")
+  $(".dropdown-menu-container").css()
+})
 $(".megamenu").mouseleave(function () {
   if (!$(".megamenu:hover").length) {
-    $(".megamenu").stop(true, true).slideUp(600);
+    $(".megamenu").stop(true, true).slideUp(600)
   }
-});
+})
 $(".dropdown-menu-container").click(function () {
-  $(".megamenu").stop(true, true).slideToggle(200);
-  $(".megamenu").css("display", "flex");
-});
-// $(".dropdown-menu-container").click(function () {
-//   $(".dropdown-button p").toggleClass("bordered");
-// });
+  $(".megamenu").stop(true, true).slideToggle(200)
+  $(".megamenu").css("display", "flex")
+})
