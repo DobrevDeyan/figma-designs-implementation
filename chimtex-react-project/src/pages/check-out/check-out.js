7597aede0bcbbd724530b823327e0e874531a54b
Dobrev <Deyan.Dobrev@BRAIN-Bulgaria.com>
2023-02-27 17:03:07 +0200

    from_work_flags

--- chimtex-react-project/src/pages/check-out/check-out.js
+++ chimtex-react-project/src/pages/check-out/check-out.js
@@ -1,54 +1,54 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import "bootstrap/dist/css/bootstrap.css"
 import "./check-out.css"
 
 function CheckOut() {
   return (
     <main id="checkout">
-      <div class="container-wrap">
-        <h4 class="page-title">Завърши поръчката</h4>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6 col-12 ">
-              <div class="box left">
-                <h4 class="box-title">Имам акаунт</h4>
-                <form class="contact-form" action="#">
-                  <div class="form-field">
-                    <label for="">E-mail aдрес*</label>
-                    <input type="text" value="" />
-                  </div>
-                  <div class="form-field">
-                    <label for="">Парола*</label>
-                    <input type="text" value="" />
-                  </div>
-                </form>
-                <div class="misc">
-                  <div class="checkbox-container">
-                    <input type="checkbox" id="checkbox" class="checkbox" />
-                    <a>Запомни ме</a>
-                  </div>
-                  <a class="forgotten_password" href="#">
-                    Забравена парола
-                  </a>
+      <div className="container-wrap container">
+        <h4 className="page-title">Завърши поръчката</h4>
+
+        <div className="row">
+          <div className="col-lg-6 col-12 justify-content-center">
+            <div className="box left">
+              <h4 className="box-title">Имам акаунт</h4>
+              <form className="contact-form" action="#">
+                <div className="form-field">
+                  <label for="">E-mail aдрес*</label>
+                  <input type="text" value="" />
                 </div>
-                <button type="button" class="show-more">
-                  <p>Вход</p>
-                </button>
-                <div class="misc-2">
-                  <p>Все още нямаш регистрация?</p>
-                  <a class="registration" href="#">
-                    Регистрирай се
-                  </a>
+                <div className="form-field">
+                  <label for="">Парола*</label>
+                  <input type="text" value="" />
                 </div>
+              </form>
+              <div className="misc">
+                <div className="checkbox-container">
+                  <input type="checkbox" id="checkbox" className="checkbox" />
+                  <a>Запомни ме</a>
+                </div>
+                <a className="forgotten_password" href="#">
+                  Забравена парола
+                </a>
+              </div>
+              <button type="button" className="show-more">
+                <p>Вход</p>
+              </button>
+              <div className="misc-2">
+                <p>Все още нямаш регистрация?</p>
+                <a className="registration" href="#">
+                  Регистрирай се
+                </a>
               </div>
             </div>
-            <div class="col-lg-6 col-12 ">
-              <div class="box right">
-                <h4 class="box-title">Продължи като гост</h4>
-                <button type="button" class="show-more">
-                  <p>Продължи</p>
-                </button>
-                {/* <img src={microscope} alt="microscope" class="hero-image" /> */}
-              </div>
+          </div>
+          <div className="col-lg-6 col-12 justify-content-center">
+            <div className="box right">
+              <h4 className="box-title">Продължи като гост</h4>
+              <button type="button" className="show-more">
+                <p>Продължи</p>
+              </button>
+              {/* <img src={microscope} alt="microscope" className="hero-image" /> */}
             </div>
           </div>
         </div>
