837c02b58eac273bf12386f9a093629391193d4b
Dobrev <Deyan.Dobrev@BRAIN-Bulgaria.com>
2023-03-09 16:51:45 +0200

    work

--- chimtex-react-project/src/components/product-card/product-card.js
+++ chimtex-react-project/src/components/product-card/product-card.js
@@ -1,12 +1,21 @@
 import "./product-card.css"
 
 function ProductCard({ id, name, price, imageUrl }) {
+  console.log("imageUrl:", imageUrl)
   return (
     <>
-      {id}
-      {name}
-      {price}
-      {imageUrl}
+      <div className="col-lg-2 col-6 hidden-card">
+        <div className="card">
+          <div className="card-image-container">
+            <img src={imageUrl} alt="gloves" className="card-image" />
+          </div>
+          <h4 className="card-title">{name}</h4>
+          <p className="card-description">
+            Латексова нестерилна ръкавица без пудра с номер {id}
+          </p>
+          <p>{price}</p>
+        </div>
+      </div>
     </>
   )
 }
--- chimtex-react-project/src/pages/promotions/promotions.js
+++ chimtex-react-project/src/pages/promotions/promotions.js
@@ -3,18 +3,20 @@ import store_items from "../../data/store.json"
 
 function Promotions() {
   return (
-    <>
+    <main id="promotions" className="container container-wrap">
       <h1>Store</h1>
-      {store_items.map((item) => (
-        <ProductCard
-          key={item.id}
-          id={item.id}
-          name={item.name}
-          price={item.price}
-          imageUrl={item.imageUrl}
-        />
-      ))}
-    </>
+      <div className="row justify-content-between">
+        {store_items.map((item) => (
+          <ProductCard
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            price={item.price}
+            imageUrl={item.imageUrl}
+          />
+        ))}
+      </div>
+    </main>
   )
 }
 
