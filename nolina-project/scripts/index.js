1797cbdf1b80d827c19a96bc053d43f69f990ff6
deyandobrev <dobrev.de@gmail.com>
2023-03-23 13:43:22 +0200

    details-done

--- nolina-project/scripts/index.js
+++ nolina-project/scripts/index.js
@@ -1,1 +1,16 @@
+// JQUERY MESSAGE SENT POP UP MODAL FUNCTIONALITY
 
+$(document).ready(function () {
+  $(".query").click(function (e) {
+    e.preventDefault()
+
+    $("#message-modal").fadeIn(1200)
+    $("#modal-overlay").fadeIn()
+    $("body").css("overflow", "hidden")
+    $(".close-modal").click(function () {
+      $("body").css("overflow", "auto")
+      $("#modal-overlay").fadeOut(500)
+      $("#message-modal").fadeOut(1200)
+    })
+  })
+})
